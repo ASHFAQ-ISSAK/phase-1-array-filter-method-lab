fcf65b7c82a1d508440bb5d5360b10079412a034
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-03-24 08:15:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+
+
+console.log(findMatching(drivers, 'Bobby'))
+
+
+
+function fuzzyMatch(drivers,name){
+    return drivers.filter(driver => driver[0] === name[0])
+}
+
+console.log(fuzzyMatch(drivers,"Bobby"))
+
+
+function matchName(drivers,name){
+
+    const findMatching= drivers.filter(driver => driver.name === name);
+
+    return findMatching
+}
+console.log(matchName(drivers,"Bobby"))
